4f36f6df97d4da8f4e9c06cce3d8f70c4f403374
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-08-21 17:53:40 -0600

    Finishing the logic. Now just trying to store the cookie value of wins and losses

--- src/components/GameButton.vue
+++ src/components/GameButton.vue
@@ -1,12 +1,29 @@
 <template>
     <div>
-        <button>Click me</button>
+        <button @click="request_number">Click me</button>
     </div>
 </template>
 
 <script>
+import axios from "axios";
     export default {
+        methods: {
+          request_number() {
+            axios.request({
+                url: `http://www.randomnumberapi.com/api/v1.0/randomnumber`
+            }).then((response) => {
+                if(response[`data`][0] >= 50) {
+                    this.$root.$emit(`win_result`, `1`);
+                } else {
+                    this.$root.$emit(`loss_result`, `1`);
+                }
+                
+            }).catch((error) => {
+                error
+            })
+        },
         
+        }
     }
 </script>
 
--- src/components/ScoreBoard.vue
+++ src/components/ScoreBoard.vue
@@ -1,13 +1,34 @@
 <template>
     <div>
-        <h3>Wins</h3>
-        <h3>Losses</h3>
+        <h3>Wins {{ win_result }}</h3>
+        <h3>Losses {{ loss_result }}</h3>
     </div>
 </template>
 
 <script>
+
     export default {
-        
+        methods: {
+            handle_win_result(number) {
+               this.win_result += parseInt(number);
+            },
+
+            handle_loss_result(number) {
+                this.loss_result += parseInt(number);
+            }
+        },
+
+        data() {
+            return {
+                win_result: 0,
+                loss_result: 0
+            }
+        },
+
+        mounted () {
+            this.$root.$on(`win_result`, this.handle_win_result);
+            this.$root.$on(`loss_result`, this.handle_loss_result);
+        },
     }
 </script>
 
