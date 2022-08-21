8ef98edbdf8d01e40d3ba5f0bf384024709a34d3
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-08-21 17:23:38 -0600

    Getting all components read to start the logic of the game

--- src/views/GamePage.vue
+++ src/views/GamePage.vue
@@ -1,15 +1,32 @@
 <template>
-    <div>
-        <h1>Hiiiii</h1>
+  <div>
+    <div v-if="token !== null">
+        <game-button></game-button>
+        <score-board></score-board>
     </div>
+
+    <div v-else>
+      <h1>To access the Game Page you should must log in.</h1>
+      <router-link to="/">Go back</router-link>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+import cookies from "vue-cookies";
+import ScoreBoard from '@/components/ScoreBoard.vue';
+import GameButton from '@/components/GameButton.vue';
+export default {
+  components: { ScoreBoard, GameButton },
+  data() {
+    return {
+      token: null,
+    };
+  },
+  mounted() {
+    this.token = cookies.get(`token`);
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
