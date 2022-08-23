f703a287cbb93482ef7e7bb5d5e72877bb93d05a
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-08-23 11:26:03 -0600

    Adding the bonus part

--- src/components/GameButton.vue
+++ src/components/GameButton.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <input type="number" ref="wager_number">
     <button @click="request_number">Click me</button>
   </div>
 </template>
@@ -13,9 +14,7 @@ export default {
   data() {
     return {
       /* setting the value of the variable to 0 */
-      win_counter: 0,
-      /* setting the value of the variable to 0 */
-      loss_counter: 0,
+      points: 100,
     };
   },
   methods: {
@@ -32,40 +31,40 @@ export default {
           /* if the response from the API is bigger and/or equal to 50 */
           if (response[`data`][0] >= 50) {
             /* if the cookie was not setted yet */
-            if (cookies.get(`win`) === null) {
-              /* add 1 to the variable win_counter */
-              this.win_counter += 1;
+            if (cookies.get(`cookie_result`) === null) {
+              /* add the value of the input to the variable points */
+              this.points += this.$refs[`wager_number`][`valueAsNumber`];
               /* emmit the the result of the sum above */
-              this.$root.$emit(`win_result`, `${this.win_counter}`);
+              this.$root.$emit(`result`, `${this.points}`);
               /* set the cookie with this new value */
-              cookies.set(`win`, `${this.win_counter}`);
+              cookies.set(`cookie_result`, `${this.points}`);
             } else {
             /* if the cookie was already setted */
-              /* get this value from the cookie and add one */
-              let value = parseInt(cookies.get(`win`)) + 1;
+              /* get this value from the cookie and add the new or the same value of the input, as the user wants */
+              let value = parseInt(cookies.get(`cookie_result`)) + this.$refs[`wager_number`][`valueAsNumber`];
               /* emmit the the result of the sum above */
-              this.$root.$emit(`win_result`, `${value}`);
+              this.$root.$emit(`result`, `${value}`);
               /* set the cookie with this new value */
-              cookies.set(`win`, `${value}`);
+              cookies.set(`cookie_result`, `${value}`);
             }
           } else {
           /* if the response from the API was less than 50 */
             /* if the cookie was not setted yet */
-            if (cookies.get(`loss`) === null) {
-              /* add 1 to the variable win_counter */
-              this.loss_counter += 1;
+            if (cookies.get(`cookie_result`) === null) {
+              /* subtract the value of the input to the variable points */
+              this.points -= this.$refs[`wager_number`][`valueAsNumber`];
               /* emmit the the result of the sum above */
-              this.$root.$emit(`loss_result`, `${this.loss_counter}`);
+              this.$root.$emit(`result`, `${this.points}`);
               /* set the cookie with this new value */
-              cookies.set(`loss`, `${this.loss_counter}`);
+              cookies.set(`cookie_result`, `${this.points}`);
             } else {
             /* if the cookie was already setted */
-              /* get this value from the cookie and add one */
-              let value = parseInt(cookies.get(`loss`)) + 1;
+              /* get this value from the cookie and subtract the new or the same value of the input, as the user wants */
+              let value = parseInt(cookies.get(`cookie_result`)) - this.$refs[`wager_number`][`valueAsNumber`];
               /* emmit the the result of the sum above */
-              this.$root.$emit(`loss_result`, `${value}`);
+              this.$root.$emit(`result`, `${value}`);
               /* set the cookie with this new value */
-              cookies.set(`loss`, `${value}`);
+              cookies.set(`cookie_result`, `${value}`);
             }
           }
         })
--- src/components/ScoreBoard.vue
+++ src/components/ScoreBoard.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-    <h3>Wins {{ win_result }}</h3>
-    <h3>Losses {{ loss_result }}</h3>
+    <h3>Wins {{ started_points }}</h3>
   </div>
 </template>
 
@@ -10,42 +9,28 @@
 import cookies from "vue-cookies";
 export default {
   methods: {
-    /* when the page loads this function is called if the the emmit that was listenned was the `win_result` */
-    handle_win_result(number) {
-      /* passing the value of the the listenner to the variable and transforming in a number since the value was a string */
-      this.win_result = parseInt(number);
-    },
-    /* when the page loads this function is called if the the emmit that was listenned was the `win_result` */
-    handle_loss_result(number) {
-      /* passing the value of the the listenner to the variable and transforming in a number since the value was a string */
-      this.loss_result = parseInt(number);
+    /* when the page loads this function is called */
+    handle_result(number) {
+      /* passing the value of the the listenner to the variable */
+      this.started_points = parseInt(number);
     },
   },
 
   data() {
     return {
-      /* setting the value to zero */
-      win_result: 0,
-      /* setting the value to zero */
-      loss_result: 0,
+      /* setting the value to 100 */
+      started_points: 100,
     };
   },
 
   mounted() {
-    /* listening the emmit `win_result and calling the function handle_win_result` */
-    this.$root.$on(`win_result`, this.handle_win_result);
-    /* listening the emmit `loss_result` and calling the function handle_loss_result */
-    this.$root.$on(`loss_result`, this.handle_loss_result);
+    /* listening the emmit `resukt` and calling the function handle_result */
+    this.$root.$on(`result`, this.handle_result);
 
     /* if the cookie was already setted, or different than null */
-    if (cookies.get(`win`) !== null) {
-      /* getting the cookie value and passing the value to the variable */
-      this.win_result = parseInt(cookies.get(`win`));
-    }
-    /* if the cookie was already setted, or different than null */
-    if (cookies.get(`loss`) !== null) {
-      /* getting the cookie value and passing the value to the variable */
-      this.loss_result = parseInt(cookies.get(`loss`));
+    if (cookies.get(`cookie_result`) !== null) {
+      /* getting the cookie value and passing the value to the variable started_points */
+      this.started_points = parseInt(cookies.get(`cookie_result`));
     }
   },
 };
